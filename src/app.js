a9173a5d128e895113035bc624f5aff2ce62d666
Guilfornari <guilfornari@gmail.com>
2023-04-15 14:48:50 -0300

    Corrected add login messages and Started requisite post messages

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import joi from "joi";
+import dayjs from "dayjs";
 //-configuration-
 const app = express();
 app.use(express.json());
@@ -19,10 +20,16 @@ try {
 const db = mongoClient.db();
 //-joi schema validations-
 const participantsSchema = joi.object({ name: joi.string().required() });
+const messagesSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid("message", "private_message").required()
+});
+
 
 //-participants-
 app.post("/participants", async (req, res) => {
-    const validation = participantsSchema.validate(req.body)
+    const validation = participantsSchema.validate(req.body);
     if (validation.error) return res.sendStatus(422);
     const { name } = req.body;
 
@@ -30,6 +37,14 @@ app.post("/participants", async (req, res) => {
         const user = await db.collection("participants").findOne({ name: name });
         if (user) return res.sendStatus(409);
         await db.collection("participants").insertOne({ name, lastStatus: Date.now() });
+        const statusMessage = {
+            from: name,
+            to: "Todos",
+            text: "entra na sala...",
+            type: "status",
+            time: dayjs().format("HH:mm:ss")
+        };
+        await db.collection("messages").insertOne(statusMessage);
         return res.sendStatus(201);
     } catch (error) {
         return res.status(500).send(error.message);
@@ -46,6 +61,36 @@ app.get("/participants", async (req, res) => {
         return res.status(500).send(error.message);
     }
 });
+
+//-messages-
+app.post("/messages", async (req, res) => {
+    const validation = messagesSchema.validate(req.body);
+    if (validation.error) return res.sendStatus(422);
+    const { to, text, type } = req.body;
+    try {
+        const chatMessage = {
+            from: "Working on it",
+            to: to,
+            text: text,
+            type: type,
+            time: dayjs().format("HH:mm:ss")
+        };
+        await db.collection("messages").insertOne(chatMessage);
+        return res.sendStatus(201);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
+
+app.get("/messages", async (req, res) => {
+    try {
+        const chatMessages = await db.collection("messages").find().toArray();
+        return res.status(200).send(chatMessages);
+
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+})
 
 //-connection-
 const PORT = 5000;
